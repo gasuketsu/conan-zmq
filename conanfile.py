0e637aa6f59a1306606dfe7366d4a8a05830c94a
tharada <harada.tomoyuki03@gmail.com>
2017-03-23 11:41:06 +0900

    remove appending LD_LIBRARY_PATH

--- conanfile.py
+++ conanfile.py
@@ -46,7 +46,6 @@ conan_basic_setup()
     def package_info(self):
         if not self.settings.os == "Windows":
             self.cpp_info.libs = ["zmq-static"] if not self.options.shared else ["zmq"]
-            self.env_info.LD_LIBRARY_PATH.append(os.path.join(self.package_folder, "lib"))
         else:
             ver = ""
             if self.settings.compiler == "Visual Studio":
